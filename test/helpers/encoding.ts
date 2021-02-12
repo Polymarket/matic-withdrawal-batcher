011deccb5a10b0c52a34bd86edee670cd51164c2
Tom French <tom@tomfren.ch>
2021-02-12 15:18:10 +0000

    refactor: allow passing BigNumberish amounts to encodeDeposit

--- test/helpers/encoding.ts
+++ test/helpers/encoding.ts
@@ -1,8 +1,8 @@
-import { BigNumber } from "@ethersproject/bignumber";
+import { BigNumber, BigNumberish } from "@ethersproject/bignumber";
 import { hexConcat, hexZeroPad } from "@ethersproject/bytes";
 
-export const encodeDeposit = (address: string, amount: BigNumber): string =>
-  hexConcat([address, hexZeroPad(amount.toHexString(), 12)]);
+export const encodeDeposit = (address: string, amount: BigNumberish): string =>
+  hexConcat([address, hexZeroPad(BigNumber.from(amount).toHexString(), 12)]);
 
 export const encodeDepositMessage = (deposits: [string, BigNumber][]): string =>
   hexConcat(deposits.map(([address, amount]) => encodeDeposit(address, amount)));
